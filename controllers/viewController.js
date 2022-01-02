1a1fe9d9b45cb07f56fde234ff8a6d525aafaae6
Kakha Gujejiani <67228459+Gujejiani@users.noreply.github.com>
2022-01-02 15:58:59 +0400

    map added using mapbox in the tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -26,8 +26,11 @@ exports.getTour = catchAsync( async(req, res)=>{
     console.log('---------------------------------------LOG')
     console.log(req.params)
     //3) render template using data from 1)
-    res.status(200).render('tour', {
-        title: 'The forest hiker',
+    res.status(200).set(
+        'Content-Security-Policy',
+        "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+      ).render('tour', {
+        title: `${tour.name} Tour`,
         tour: tour
     })
 })
---
+++ public/js/mapbox.js
@@ -0,0 +1,50 @@
+console.log('hello fom client sucker')
+/* eslint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations)
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2FraGE3IiwiYSI6ImNreHg1c3FiNjRidGkyeXFrMjRnamoxZWUifQ.1znh8ROPtZw0rxKhfFoBKQ';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/kakha7/ckxx66hxm1vi615mu2z50s8om',
+scrollZoom: false,
+// center: [-118.333, 123.323],
+// zoom: 5,
+// interactive: false
+});
+
+
+const bounds = new mapboxgl.LngLatBounds();
+
+
+locations.forEach(loc=>{
+    //create marker
+
+    const el = document.createElement('div')
+    el.className='marker';
+
+     // add marker
+    new mapboxgl.Marker({
+        element: el,
+        anchor: 'bottom'
+    }).setLngLat(loc.coordinates).addTo(map)
+
+
+    // add popup
+    new mapboxgl.Popup({
+        offset:30
+    }).setLngLat(loc.coordinates).setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map)
+
+  // Extend map bouns to include current location
+    bounds.extend(loc.coordinates)
+})
+
+
+map.fitBounds(bounds,{
+    padding: {
+        top: 200,
+        bottom: 50,
+        left:  100,
+        right: 100
+    }
+ 
+})
