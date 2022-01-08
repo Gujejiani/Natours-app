4bf2ec4dcbc234aa801be89df669d431ee27f73b
Kakha Gujejiani <67228459+Gujejiani@users.noreply.github.com>
2022-01-08 13:38:26 +0400

    bug fixed

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -22,7 +22,7 @@ exports.getCHeckoutSession = catchAsync(async (req, res, next)=>{
             {
                name: `${tour.name} Tour`,
                description: tour.summary,
-               images: [`https://wwww.natours.dev/img/tours/${tour.imageCover}`],
+               images: [`${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`],
                amount: tour.price* 100,
                currency: 'usd',
                quantity: 1
@@ -57,8 +57,11 @@ exports.getCHeckoutSession = catchAsync(async (req, res, next)=>{
 
 const createBookingCheckout = async  session => {
     const tour = session.client_reference_id
-    const user = (await User.findOne({email: session.customer_email})).id
+    const user = (await User.findOne({email: session.customer_email}))._id
     const price = session.line_items[0].amount / 100
+
+    console.log('thouse gyuyssssssssssssssssssssssssssssssssss')
+    console.log(tour, user, price)
     await Booking.create({
         tour,
         user,
